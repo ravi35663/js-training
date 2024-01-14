3ce4d3605f01f9eed40bc7408b5fc1706fd3946e
ravi <ravikr845430@gmail.com>
2024-01-14 16:36:00 +0530

    datatypes and declaration done

---
+++ app.js
@@ -0,0 +1,58 @@
+//Variables and Data Types:
+
+/*
+    Declaration in JS:
+    1) var : 
+    2) let
+    3) const
+    let and const introduced in modern javascript (ES-6) (ES-2015)
+*/ 
+
+// var
+// console.log("My Name is  line 14",myName);
+// myName = "Ravi"
+// console.log("My name is ",myName);
+
+// console.log("My variable ",myAge); // undefined
+// var myAge = 25;// assigned a block [] // myAge -> 25
+// console.log("My age is ",myAge); // undefined -> 25
+
+// let
+let myName = "Mohit" // 
+console.log("My name is ",myName);// 
+
+myName = "Jitender";
+console.log("My name is ",myName);
+
+// const : only assigned during initialization phase (only for primitive datatypes)
+// const myAge = 25;
+// console.log("My age is ",myAge);
+// myAge++; 
+
+/**
+ * Data Types
+ * 1) Primitive data types 
+ * 2) Non-Primitive Data types or user-defined data type :
+ * 3) Compound Data types: formed using primitive or non-primitive data types
+ */ 
+ 
+// 1 =>  ,01
+
+/*
+    Primitive data type:
+    1) String :- anything between double or single quote would be string ("name","1234a")
+    2) Number or Integer: any number would be number or integer .( -1,0,3, 0.1)
+    3) Boolean : true or false
+    4) null : 
+    5) undefined :  
+*/
+
+/*
+    Compound Data types:
+    1) Array : [1,2,"Rav","ravi@gmail.com",true]; 
+    2) Object : {name:"Ravi",age:25,isMarried:false};
+    3) Set : set([1,23,"ravi"]);
+        ..etc
+*/
+
+
